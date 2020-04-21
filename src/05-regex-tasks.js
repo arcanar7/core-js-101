5ab0e5e7f7ad43cd2e7d25c8658f4faf7b496cfa
arcanar7 <arcanar7@gmail.com>
2020-04-21 18:12:02 +0400

    feat: add regex tasks solution

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -8,7 +8,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Returns the regexp that matches a GUID string representation
  * '{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}',
@@ -32,9 +31,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}/;
 }
-
 
 /**
  * Returns the regexp that matches all the strings from first column
@@ -54,9 +52,8 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p[a-z|\s]t/;
 }
-
 
 /**
  * Returns the password validator regex.
@@ -78,10 +75,11 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  return new RegExp(
+    `^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{${minLength},}$`,
+  );
 }
-
 
 module.exports = {
   getRegexForGuid,
